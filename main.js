28d91b34faf5b626f28c231e04eae176e30ebb42
Abdul Kabeer <161349706+Kabeerrazzaq@users.noreply.github.com>
2024-05-11 02:12:01 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-var num = 2;
-console.log("value of num:", "", "my lucky number", "", num);
-num = 12;
-console.log(num);
+// Arry
+// let Mybakry : string[] = ["biscit" , "doodh" , "nimco", "slice"];
+// let studentNames : string[]= ["ahmad" , "kashif" , "mohid" , "zeeshan"];
+// let student = studentNames.indexOf
+// console.log(student);
+// let startArry =[];
+// startArry[0]= "student";
+// console.log(startArry);
+// let myFruit : string[] = ["apple", "orange", "mango" , "leachi"]
+// myFruit[3]= "banana"
+// myFruit.pop();
+// myFruit.push("banana", "stawbery" , "leachi")
+// myFruit.shift();
+// myFruit.unshift("pineapple" , "cherry");
+// let Con_cate : string[] = Mybakry.concat(myFruit);
+// console.log(Con_cate);
+var Allowedstuden = ["kashif", "mohid", "moiz", "amaar", "extra", "ahmad", "ahad", "samaad",];
+// if(Allowedstuden => 5){
+//     Allowedstuden.push("mubeen");
+// }
+// console.log(Allowedstuden);
+// let Fllowedstuden= Allowedstuden.slice(0,8)
+// console.log(Fllowedstuden);
+var f_Allowedstuden = Allowedstuden.splice(0, 3, 'junaid');
+console.log(f_Allowedstuden);
--- main.ts
+++ main.ts
@@ -1,4 +1,36 @@
-var num = 2
-console.log("value of num:", "" , "my lucky number", "", num);
-num = 12
-console.log(num);
+// Arry
+
+ let Mybakry : string[] = ["biscit" , "doodh" , "nimco", "slice"];
+
+let studentNames : string[]= ["ahmad" , "kashif" , "mohid" , "zeeshan"];
+
+let student = studentNames.indexOf
+console.log(student);
+
+let startArry : string =[];
+startArry[0]  = "student";
+console.log(startArry);
+
+let myFruit : string[] = ["apple", "orange", "mango" , "leachi"]
+myFruit[3]= "banana"
+myFruit.pop();
+myFruit.push("banana", "stawbery" , "leachi")
+myFruit.shift();
+myFruit.unshift("pineapple" , "cherry");
+
+let Con_cate : string[] = Mybakry.concat(myFruit);
+console.log(Con_cate);
+
+let Allowedstuden:string[] = ["kashif" , "mohid", "moiz", "amaar" , "extra" ,"ahmad", "ahad", "samaad",]
+if(Allowedstuden => 5){
+    Allowedstuden.push("mubeen");
+}
+console.log(Allowedstuden);
+
+
+let Fllowedstuden= Allowedstuden.slice(0,8)
+console.log(Fllowedstuden);
+let f_Allowedstuden : string[] = Allowedstuden.splice(0,3,)
+console.log(f_Allowedstuden);
+
+
